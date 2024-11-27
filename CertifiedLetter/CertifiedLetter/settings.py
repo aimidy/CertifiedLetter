c4960a1b0108e7390240f502a1804a43faf7d46e
Changfu.Lin <changfu.lin@xsgames.com>
2024-11-27 11:09:57 +0800

    更新設定檔以允許所有主機並新增 uWSGI 配置檔

--- CertifiedLetter/CertifiedLetter/settings.py
+++ CertifiedLetter/CertifiedLetter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ojv#e@c70jq76m_3wxemx)6br1mx0y)59ppfouqmm3!(k2i!9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'CertifiedLetter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
 
 
 # Password validation
